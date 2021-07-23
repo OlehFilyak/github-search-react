e379563f7a046c36fe9d4eb7c9c5e33d12c907d9
Олег Філяк <73895897+OlehFilyak@users.noreply.github.com>
2021-07-23 18:28:06 +0300

    added structure to the project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import "./App.css";
+import React from "react";
+import { Navbar } from "./Components/Navbar/Navbar";
+import { Home } from "./Components/pages/Home";
+import "./App.scss";
 
-function App() {
-  return <div className="App"></div>;
+import { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <div className="App">
+          <Home />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+export const Navbar = () => (
+  <nav className="navbar navbar-dark bg-primary navbar-expand-lg">
+    <div className="navbar-brand">Github Search</div>
+    <ul className="navbar-nav">
+      <li className="nav-item">
+        <a href="/" className="nav-link">
+          Main
+        </a>
+      </li>
+      <li className="nav-item">
+        <a href="/about" className="nav-link">
+          Profile
+        </a>
+      </li>
+    </ul>
+  </nav>
+);
